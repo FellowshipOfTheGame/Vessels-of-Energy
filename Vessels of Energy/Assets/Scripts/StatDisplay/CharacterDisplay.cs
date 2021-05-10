319bfb797bcc0dfc5e57cb1005b9fd6a7f085151
Leodewill <leode.g.will@gmail.com>
2021-05-10 18:01:53 -0300

    Adding HUD from other branch

---
+++ Vessels of Energy/Assets/Scripts/StatDisplay/CharacterDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterDisplay : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Gradient healthGradient;
+    public Image healthFill;
+    public Text staminaValue;
+
+    void Update()
+    {
+        Character c = GetComponent<Character>();
+        //TODO: Test Health Bar
+        healthSlider.maxValue = c.maxHP;
+        healthSlider.value = c.HP;
+
+        //Changes bar color depending on the % of health
+        healthFill.color = healthGradient.Evaluate(healthSlider.normalizedValue);
+
+        staminaValue.text = c.stamina.ToString();
+    }
+}
---
+++ Vessels of Energy/Assets/Scripts/StatDisplay/TargetDisplay.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetDisplay : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Gradient healthGradient;
+    public Image healthFill;
+
+    public Text staminaValue;
+    /*public Text strengthValue;
+    public Text evasionValue;*/
+    public Text defenseValue;
+    public Text proficiencyDice;
+    public Text damageDice;
+
+    void Update()
+    {
+        if (Character.target != null){
+            Character c = Character.target;
+
+            healthSlider.maxValue = c.maxHP;
+            healthSlider.value = c.HP;
+
+            //Changes bar color depending on the % of health
+            healthFill.color = healthGradient.Evaluate(healthSlider.normalizedValue);
+
+            staminaValue.text = c.stamina.ToString();
+            /*strengthValue.text = c.strength.ToString();
+            evasionValue.text = c.evasion.ToString();*/
+            defenseValue.text = c.defense.ToString();
+
+            //TODO: Get these information from Character and Character's Weapon
+            proficiencyDice.text = "1d8";
+            damageDice.text = "1d12";
+        }
+        else{
+            healthSlider.maxValue = 1;
+            healthSlider.value = 0;
+
+            staminaValue.text = "";
+            /*strengthValue.text = "";
+            evasionValue.text = "";*/
+            defenseValue.text = "";
+            proficiencyDice.text = "";
+            damageDice.text = "";
+        }
+    }
+}
