b6e8278efad586e8f01f0f394aafed498003e8c9
Amandou <amanda.lrib@gmail.com>
2021-11-04 23:44:55 -0300

    Base Medium distance attack

--- Vessels of Energy/Assets/Scripts/Character/Sorcerer.cs
+++ Vessels of Energy/Assets/Scripts/Character/Sorcerer.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Sorcerer : Character
 {
+    private int minRange = 2, maxRange = 4;
+    public GameObject bullet;
+    //public ParticleSystem attackParticle;
+
     void Start()
     {
         //Stats still need to be balanced
@@ -17,12 +21,40 @@ public class Sorcerer : Character
         this.calculateStats();
     }
 
-    public override void Action(){
+    public override void Action()
+    {
         Debug.Log("Sorcerer Action");
 
-        /*if(stamina == 0){
+        if (target.team != GameManager.currentTeam)
+        {
+            if (this.stamina >= ATTACK_COST && target.HP >= 0)
+            {
+                target.place.changeState("enemy");
+                this.MediumDistanceAttack(target, minRange, maxRange);
+            }
+            else
+            {
+                Debug.Log("Not enough stamina");
+            }
+        }
+        else
+        {
             locked = false;
-        }*/
-    }
+            target.Select();
+        }
 
+        if (stamina == 0)
+        {
+            locked = false;
+        }
+    }
+    public void MediumDistanceAttack(Character target, int minRange, int maxRange)
+    {
+        Debug.Log("Medium Distance Attack");
+        if (checkRange(minRange, maxRange))
+        {
+            this.stamina -= Character.ATTACK_COST;
+            target.HP -= Character.ATTACK_COST;
+        }
+    }
 }
