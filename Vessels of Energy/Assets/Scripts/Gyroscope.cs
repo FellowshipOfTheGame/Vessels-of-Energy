e5507b8f12eff0fe2b82dff23dcd212f52793d57
Will Zagario <leode.g.will@gmail.com>
2022-01-11 20:45:52 -0300

    Add Healer Prefab in Game

--- Vessels of Energy/Assets/Scripts/Gyroscope.cs
+++ Vessels of Energy/Assets/Scripts/Gyroscope.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gyroscope : MonoBehaviour {
-    class Object2D {
+public class Gyroscope : MonoBehaviour
+{
+    class Object2D
+    {
         public Renderer renderer;
         public int order;
 
-        public Object2D(Renderer renderer) {
+        public Object2D(Renderer renderer)
+        {
             this.renderer = renderer;
             this.order = renderer.sortingOrder;
         }
@@ -16,17 +19,19 @@ public class Gyroscope : MonoBehaviour {
     List<Object2D> objects;
     int margin = 40;
     public int order = 0;
-    public List<Renderer> renderers;
+    [HideInInspector] public List<Renderer> renderers;
 
     public static Transform cam;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         objects = new List<Object2D>();
 
         renderers = new List<Renderer>();
         renderers = GetAllRenderers(this.transform, renderers);
-        foreach (Renderer r in renderers) {
+        foreach (Renderer r in renderers)
+        {
             objects.Add(new Object2D(r));
         }
 
@@ -39,17 +44,21 @@ public class Gyroscope : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         this.transform.forward = new Vector3(cam.forward.x, this.transform.forward.y, cam.forward.z);
-        foreach (Object2D obj in objects) {
+        foreach (Object2D obj in objects)
+        {
             int distance = (int)((cam.position - this.transform.position).magnitude * 10f);
             distance = Mathf.Clamp(distance, 0, 300);
             obj.renderer.sortingOrder = obj.order + (300 - distance) * margin + order * margin / 2;
         }
     }
 
-    List<Renderer> GetAllRenderers(Transform t, List<Renderer> list) {
-        for (int i = 0; i < t.childCount; i++) {
+    List<Renderer> GetAllRenderers(Transform t, List<Renderer> list)
+    {
+        for (int i = 0; i < t.childCount; i++)
+        {
             Transform child = t.GetChild(i);
             Renderer r = child.gameObject.GetComponent<Renderer>();
             if (r != null) list.Add(r);
